fbda032bf7ca08b95bcfbce04ba2946f115d738c
Jim Reed <cjoackin@gmail.com>
2021-03-12 17:33:45 -0300

    Added cors headers

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -11,6 +12,7 @@ const productsRouter = require('./routes/products');
 const categoriesRouter = require('./routes/categories');
 
 const app = express();
+
 
 const db = require('./config/keys').mongoURI;
 
@@ -33,15 +35,28 @@ app.use(bodyParser.json());
 app.set('query parser', 'simple');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PUT');                                                                        
+  next();                      
+});
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With,x-access-token');                 
+  res.send(200);
+});
+
+// app.use(cors());
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
 require('./config/passport')(passport);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/categories', categoriesRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/categories', categoriesRouter);
 
 const port = process.env.PORT || 5000;
 
